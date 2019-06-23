37945c0b667c0cc99a0aaee4e88a645b694ced7d
Bengt Marten Agren <marangisto@me.com>
2019-06-23 18:42:48 +0800

    Refactored emulator code for reuse.

---
+++ emulator/Main.cpp
@@ -0,0 +1,57 @@
+#include <text.h>
+#include <fontlib.h>
+#include "Display.h"
+
+using namespace fontlib;
+using namespace color;
+
+typedef display_t<240, 240> display;
+
+void run()
+{
+    display::initialize(2);
+    display::clear(blue);
+
+    bool quit = false;
+    font_t font = fontlib::cmunrm_48;
+    st7789::text_renderer_t<display> tr(font, white, red, true);
+
+    uint16_t r = font.start_row();
+
+    tr.set_pos(0, r);
+
+    while (!quit)
+    {
+        char c;
+
+        if (keyboard_poll(c))
+            switch (c)
+            {
+            case 0:
+                quit = true;
+                break;
+            case '\r':
+                r += font.line_spacing();
+                tr.set_pos(0, r);
+                break;
+            default:
+                tr.write(c);
+                display::render();
+            }
+    }
+
+    display::shutdown();
+}
+
+int main()
+{
+    try
+    {
+        run();
+    }
+    catch (const char *s)
+    {
+        printf("exception: %s\n", s);
+    }
+}
+
