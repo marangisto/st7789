8b2a9a3dec5d79c97a75e1e4fc436c19995795a6
Bengt Marten Agren <marangisto@me.com>
2019-06-23 21:00:58 +0800

    Corrected rectangle width and height.

--- include/draw.h
+++ include/draw.h
@@ -86,8 +86,8 @@ public:
     inline void rectangle(int16_t x, int16_t y, int16_t w, int16_t h)
     {
         move_to(x, y);
-        hline_to(x + w);
-        vline_to(y + h);
+        hline_to(x + w - 1);
+        vline_to(y + h - 1);
         hline_to(x);
         vline_to(y);
     }
