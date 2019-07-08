a462dcff58fd1f7a5bad9f7637ec5f1e69e5aad2
Bengt Marten Agren <marangisto@me.com>
2019-07-08 21:51:49 +0800

    Added missing update callbacks.

--- emulator/Main.cpp
+++ emulator/Main.cpp
@@ -21,16 +21,16 @@ void run()
 
     bool quit = false;
 
-    widget_t<display, unit_t> b0(fontlib::cmunss_20, yellow, crimson, 50, 50, 100, 25, [](auto _) { return "-"; }, true);
-    widget_t<display, unit_t> b1(fontlib::cmunss_20, yellow, rebecca_purple, 50, 100, 100, 25, [](auto _) { return "Hello World!"; }, true);
-    widget_t<display, unit_t> b2(fontlib::cmunss_20, yellow, olive_drab, 130, 150, 10, 15, [](auto _) { return "-----H j-g-----"; }, true);
-    widget_t<display, unit_t> b3(fontlib::cmunss_20, yellow, lime_green, 20, 200, 50, 25, [](auto _) { return "//"; }, true);
-    widget_t<display, unit_t> b4(fontlib::cmunss_20, yellow, orange_red, 130, 200, 50, 25, [](auto _) { return "l//g"; }, true);
+    widget_t<display, unit_t> b0(fontlib::cmunss_20, yellow, crimson, 50, 50, 100, 25, [](auto _) { return "-"; }, 0, true);
+    widget_t<display, unit_t> b1(fontlib::cmunss_20, yellow, rebecca_purple, 50, 100, 100, 25, [](auto _) { return "Hello World!"; }, 0, true);
+    widget_t<display, unit_t> b2(fontlib::cmunss_20, yellow, olive_drab, 130, 150, 10, 15, [](auto _) { return "-----H j-g-----"; }, 0, true);
+    widget_t<display, unit_t> b3(fontlib::cmunss_20, yellow, lime_green, 20, 200, 50, 25, [](auto _) { return "//"; }, 0, true);
+    widget_t<display, unit_t> b4(fontlib::cmunss_20, yellow, orange_red, 130, 200, 50, 25, [](auto _) { return "l//g"; }, 0, true);
 
     display::render();
 
-    widget_t<display, char*> txbox(fontlib::cmunrm_48, dim_gray, wheat, 160, 50, 50, 40, [](auto s) { return s; });
-    widget_t<display, int> ibox(fontlib::cmuntt_24, white, web_gray, 10, 10, 50, 30, [](int x) { sprintf(tmp_buf, "%d", x); return tmp_buf; }, true);
+    widget_t<display, char*> txbox(fontlib::cmunrm_48, dim_gray, wheat, 160, 50, 50, 40, [](auto s) { return s; }, 0);
+    widget_t<display, int> ibox(fontlib::cmuntt_24, white, web_gray, 10, 10, 50, 30, [](int x) { sprintf(tmp_buf, "%d", x); return tmp_buf; },0,  true);
 
     while (!quit)
     {
