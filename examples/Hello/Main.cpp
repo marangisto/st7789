06fde9fd99f324d4d773ded3e308d6ce52d11638
Bengt Marten Agren <maraingist@me.com>
2019-09-23 21:15:15 +0800

    Added mapping for BLUE-PILL.

--- examples/Hello/Main.cpp
+++ examples/Hello/Main.cpp
@@ -21,6 +21,9 @@ static const spi::spi_clock_divider_t display_spi_prescale = spi::fpclk_2;
 #elif defined(STM32G431)
 typedef st7789_t<1, PA5, PA7, PC7, PB6> display;    // NUCLEO-STM32G431
 static const spi::spi_clock_divider_t display_spi_prescale = spi::fpclk_8;
+#elif defined(STM32F103)
+typedef st7789_t<1, PA5, PA7, PA4, PA3> display;    // BLUE-PILL
+static const spi::spi_clock_divider_t display_spi_prescale = spi::fpclk_32;
 #endif
 
 typedef output_t<PC8> led_a;
