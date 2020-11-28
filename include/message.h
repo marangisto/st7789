e43fa9058fcde602b9311e26170c27b82133b3bf
Bengt Marten Agren <marangisto@me.com>
2020-11-28 14:16:23 +0800

    Button press message is int to allow encoding release events.

--- include/message.h
+++ include/message.h
@@ -7,5 +7,5 @@ enum unit_t { unit };   // FIXME: define elsewhere!
 
 enum message_tag_t { button_press, encoder_delta, encoder_press, aux_data };
 
-typedef std::variant<uint8_t, int16_t, unit_t, uint32_t> message_t;
+typedef std::variant<int8_t, int16_t, unit_t, uint32_t> message_t;
 
